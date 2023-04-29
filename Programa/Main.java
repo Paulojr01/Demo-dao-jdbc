27948fd8bacda7a433c05bc8ce789ffcd059690b
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-04-29 16:59:11 -0300

    Criado busca por departamento
    Criado Busca de vendedor por departamento

--- Programa/Main.java
+++ Programa/Main.java
@@ -6,14 +6,27 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o numero do departamento para consultar vendedor :");
+        int id = ler.nextInt();
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("=== Test1: seller findyById ====");
-        Seller seller = sellerDao.findById(2);
-
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("=== Test2: seller findyByDepartmentId ====");
+        Department department = new Department(id,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
